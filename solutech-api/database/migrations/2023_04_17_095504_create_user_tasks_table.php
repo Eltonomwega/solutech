ee73e67af4a6459cac2aa92edcacb262ca758ff4
elton <ltomwega@gmail.com>
2023-04-17 13:35:01 +0300

    Feature: Database setup

---
+++ solutech-api/database/migrations/2023_04_17_095504_create_user_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->foreignId('task_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->timestamp('due_date');
+            $table->timestamp('start_time');
+            $table->timestamp('end_time');
+            $table->string('remarks',100);
+            $table->foreignId('status_id')
+                  ->constrained('status')
+                  ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_tasks');
+    }
+};
