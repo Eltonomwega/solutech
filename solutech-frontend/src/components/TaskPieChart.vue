5f2e1d45c3d59fae5e4178f88f108c851e050039
elton <ltomwega@gmail.com>
2023-04-21 18:51:12 +0300

    Fix: Chart

--- solutech-frontend/src/components/TaskPieChart.vue
+++ solutech-frontend/src/components/TaskPieChart.vue
@@ -16,29 +16,52 @@ export default {
     },
   },
   mounted() {
-    const pendingTasks = this.tasks.filter((task) => task.status === "Pending");
-    const completeTasks = this.tasks.filter(
-      (task) => task.status === "Complete"
-    );
-    const activeTasks = this.tasks.filter((task) => task.status === "Active");
-    console.log(pendingTasks);
-    const pieChart = new Chart(this.$refs.pieChart, {
-      type: "pie",
-      data: {
-        labels: ["Pending", "Complete", "Active"],
-        datasets: [
-          {
-            label: "Tasks",
-            data: [3, 2, 6],
-            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    });
+    this.drawChart();
+  },
+  methods: {
+    drawChart() {
+      const pendingTasks = this.tasks.filter(
+        (task) => task.status === "Pending"
+      );
+      const completeTasks = this.tasks.filter(
+        (task) => task.status === "Complete"
+      );
+      const activeTasks = this.tasks.filter((task) => task.status === "Active");
+      const pieChart = new Chart(this.$refs.pieChart, {
+        type: "pie",
+        data: {
+          labels: ["Pending", "Complete", "Active"],
+          datasets: [
+            {
+              label: "Tasks",
+              data: [
+                pendingTasks.length,
+                completeTasks.length,
+                activeTasks.length,
+              ],
+              backgroundColor: ["#FFCE56", "#42b883", "#36A2EB"],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+        },
+      });
+      this.pieChart = pieChart;
+    },
+  },
+  watch: {
+    tasks() {
+      this.pieChart.destroy(); // destroy the old chart
+      this.drawChart(); // draw a new chart with updated tasks
+    },
+  },
+  destroyed() {
+    // destroy the chart to prevent memory leaks
+    if (this.pieChart) {
+      this.pieChart.destroy();
+    }
   },
 };
 </script>
